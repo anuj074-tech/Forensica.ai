493bfc665cc4ad1765357d709ecb6509e362e9ca
anuj074-tech <redsleve074@gmail.com>
2025-10-16 20:53:23 +0530

    Delete .next directory

--- .next/static/chunks/src_app_case_[caseId]_processing_page_tsx_4390fbcb._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_65fdd258._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_page_tsx_34080300._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_1d28fd12._.js",
-  "static/chunks/src_08485830._.js"
-],
-    source: "dynamic"
-});
