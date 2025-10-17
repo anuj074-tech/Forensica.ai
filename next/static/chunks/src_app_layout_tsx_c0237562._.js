70a9d711da090226917db3124ed81c8fcdc2079a
anuj074-tech <redsleve074@gmail.com>
2025-10-17 15:26:03 +0530

    Delete next directory

--- next/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_globals_b805903d.css",
-  "static/chunks/src_2f368b72._.js",
-  "static/chunks/node_modules_01595c96._.js"
-],
-    source: "dynamic"
-});
